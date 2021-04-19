87e0863b6b4fd8320caf1524492547ed97c68a65
Erica Xia <ericaxia3@gmail.com>
2021-04-19 16:23:20 -0700

    add review view

---
+++ usc-films-cp/Views/ReviewView.swift
@@ -0,0 +1,20 @@
+//
+//  ReviewView.swift
+//  usc-films-cp
+//
+//  Created by Erica Xia on 4/19/21.
+//
+
+import SwiftUI
+
+struct ReviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ReviewView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReviewView()
+    }
+}
