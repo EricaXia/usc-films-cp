3e488b12731405ddb88ca61299048ad7c3dafa06
Erica Xia <ericaxia3@gmail.com>
2021-04-24 11:52:09 -0700

    search appearnce

--- usc-films-cp/Views/SearchView.swift
+++ usc-films-cp/Views/SearchView.swift
@@ -8,8 +8,50 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State private var searchQuery = ""
+    
     var body: some View {
-        Text("Search view")
+        
+        ScrollView {
+            
+            VStack(alignment: .leading) {
+                Spacer()
+                Text("Search")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(.leading)
+                    .padding(.top, 20)
+                    
+                    
+            
+            HStack {
+                TextField("Search Movies, TVs...", text: $searchQuery)
+                    .padding([.top, .bottom, .trailing], 5)
+                    .padding(.leading, 30)
+                    .frame(height: 40.0)
+                    .foregroundColor(Color(.systemGray2))
+                    
+            }
+            .background(Color(.systemGray5))
+            .cornerRadius(10)
+            .padding(.horizontal, 8.0)
+            .overlay(
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                        .foregroundColor(.gray)
+                    Spacer()
+                }
+                .padding(.horizontal)
+            )
+            
+            
+            Spacer()
+                
+                } //vstack
+            
+        } //scrollview
+        
+        
     }
 }
 
