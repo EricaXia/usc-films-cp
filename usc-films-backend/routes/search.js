71dccc97437090c49d60d0747892d5ae740fe791
Erica Xia <ericaxia3@gmail.com>
2021-04-26 11:36:14 -0700

    update search BE

--- usc-films-backend/routes/search.js
+++ usc-films-backend/routes/search.js
@@ -5,9 +5,7 @@ let searchRouter = express.Router({ mergeParams: true });
 
 const API_KEY = "a0f44b5888d8f94e608f47c1eb5575a4";
 
-/* Send data to details page*/
 searchRouter.get("/", (req, res) => {
-    // const search_id = req.params.search_id;
     const query = req.params.query;
     const url = "https://api.themoviedb.org/3/search/multi?api_key=" + API_KEY + "&language=en-US&page=1&query=" + query;
     axios
@@ -21,28 +19,32 @@ searchRouter.get("/", (req, res) => {
                     search_data[i]['title'] = search_data[i]['name']
                 }
 
+                // skip results that DON"T have backdrop image
                 if (search_data[i]["backdrop_path"]) {
                     search_data[i]["img_path"] = "https://image.tmdb.org/t/p/w500" + search_data[i]["backdrop_path"];
 
-                    if (search_data[i]["media_type"] === "movie") {
-                        search_data[i]["weblink"] = "/watch/movie/" + search_data[i]["id"];
-                    } else if (search_data[i]["media_type"] === "tv") {
-                        search_data[i]["weblink"] = "/watch/tv/" + search_data[i]["id"];
-                    }
+                    // Placeholder data, TODO: get actual values
+                    search_data[i]["year"] = "2021"
+                    search_data[i]["star_rating"] = 2.5
 
                     search_data2.push({
-                        "img_path": search_data[i]["img_path"],
-                        "title": search_data[i]["title"],
                         "id": search_data[i]["id"],
-                        "weblink": search_data[i]["weblink"]
+                        "title": search_data[i]["title"],
+                        "year": search_data[i]["year"],
+                        "star_rating": search_data[i]["star_rating"],
+                        "img_path": search_data[i]["img_path"]
                     });
                 }
 
-            }
+            } // end for loop
 
-            let search_data3 = search_data2.slice(0,10);
+            // If returning top 10 results only
+            // let search_data3 = search_data2.slice(0,10);
 
-            res.send(search_data3);
+            res.json({
+                results: [search_data2]
+              });
+
         })
         .catch((error) => console.log(error));
 });
