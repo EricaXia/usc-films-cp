601ba6475e3f29aca670108a01899f87ecdcaf7d
Erica Xia <ericaxia3@gmail.com>
2021-04-26 10:42:20 -0700

    add search downloader swift

---
+++ usc-films-cp/Network/SearchDownloader.swift
@@ -0,0 +1,42 @@
+//
+//  SearchDownloader.swift
+//  usc-films
+//
+//  Created by Erica Xia on 4/26/21.
+//
+
+import SwiftUI
+
+final class SearchDownloader: ObservableObject{
+    @Published var movieD = [MovieD]()
+    
+    private var searchText: String
+//    static var baseURL = "http://localhost:8080/apis/watch/"
+    static var baseURL = "http://uscfilmsbackend-env.eba-gpz54xj7.us-east-2.elasticbeanstalk.com/apis/search/"
+    
+    init(searchText: String) {
+        self.searchText = searchText
+    }
+    
+    func getSearchResults() {
+        print("Get Search Results from backend")
+        getSearchResultsData(for: searchText)
+        
+    }
+
+    private func getSearchResultsData(for searchText: String) {
+        let urlString = "\(Self.baseURL)/\(self.searchText)"
+        print("URLString:")
+        print(urlString)
+        NetworkManager<MovieDetailsResponse>.fetchData(from: urlString) { (result) in
+            switch result {
+            case .success(let movieDetailsResponse):
+                self.movieD = movieDetailsResponse.results
+//                 print(self.movieD) //WORKS here
+            case .failure(let err):
+                print(err)
+            }
+        }
+        
+    }
+}
